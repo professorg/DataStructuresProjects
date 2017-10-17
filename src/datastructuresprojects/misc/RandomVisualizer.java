ca5f9aaa567caa25d03b8992eb4bbdd9ac5078bb
ProfessorG <grant.vandomelen@gmail.com>
2017-10-17 00:05:43 -0700

    random

--- src/datastructuresprojects/misc/RandomVisualizer.java
+++ src/datastructuresprojects/misc/RandomVisualizer.java
@@ -14,6 +14,8 @@ import edu.princeton.cs.algs4.StdRandom;
  */
 public class RandomVisualizer {
     
+    public static final int DELAY = 100;
+    
     public static void main(String[] args) {
         
         int lo = 0;
@@ -44,15 +46,28 @@ public class RandomVisualizer {
             StdDraw.filledRectangle(n/2.0, nums/2.0, n/2.0, nums/2.0);
             
             int r = StdRandom.uniform(lo, hi);
+            //int r = (int)(Math.random()*n+lo);
             count[r]++;
+            
+            StdDraw.textRight(1.0*n, 1.025*nums, "nums = " + nums);
+            
+            for (int i = 0; i < n; i++) {
+                StdDraw.text(i+0.5, -0.025*nums, ""+(i+lo));
+            }
             
             StdDraw.setPenColor(StdDraw.WHITE);
             
             for (int i = 0; i < n; i++) {
-                StdDraw.filledRectangle(i+0.5, count[i+lo]/2.0, 0.45, count[i+lo]);
+                StdDraw.filledRectangle(i+0.5, count[i+lo]/2.0, 0.45, count[i+lo]/2.0);
+                StdDraw.text(i+0.5, count[i+lo]/2.0+0.1*nums, ""+count[i+lo]);
             }
             
+            StdDraw.setPenColor(StdDraw.GRAY);
+            
+            StdDraw.filledRectangle(n/2.0, (double)(nums)/n, n/2.0, 0.0025*nums);
+            
             StdDraw.show();
+            StdDraw.pause(DELAY);
         }
     }
 }
