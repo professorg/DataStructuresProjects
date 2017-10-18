ca2cc621a284ab47b99ad91d97403eb19894e56b
gvandomelen19 <gvandomelen19@S-RM44-07.jesuitpdx.org>
2017-10-18 09:41:01 -0700

    Plot

--- src/datastructuresprojects/misc/Plot.java
+++ src/datastructuresprojects/misc/Plot.java
@@ -6,6 +6,7 @@
 package datastructuresprojects.misc;
 
 import edu.princeton.cs.algs4.StdDraw;
+import java.util.function.Function;
 
 /**
  *
@@ -13,16 +14,88 @@ import edu.princeton.cs.algs4.StdDraw;
  */
 public class Plot {
     
-    public Plot(double xLo, double xHi, double yLo, double yHi) {
-        init(xLo, xHi, yLo, yHi);
+    private double xLo;
+    private double yLo;
+    private double xHi;
+    private double yHi;
+    
+    public Plot() {
+        this(0.0, 0.0, 1.0, 1.0);
     }
     
-    public void init(double xLo, double xHi, double yLo, double yHi) {
+    public Plot(double xLo, double yLo, double xHi, double yHi) {
+        this.xLo = xLo;
+        this.yLo = yLo;
+        this.xHi = xHi;
+        this.yHi = yHi;
+        init();
+    }
+    
+    private void init() {
+        double dx = xHi-xLo;
+        double dy = yHi-yLo;
         StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(-0.05*xLo, 1.05*xHi);
-        StdDraw.setYscale(-0.05*yLo, 1.05*yHi);
-        StdDraw.rectangle((xHi-xLo)/2.0, (yHi-yLo)/2.0,
-                          (xHi-xLo)/2.0, (yHi-yLo)/2.0);
+        StdDraw.setXscale(-0.05*dx + xLo, 1.05*dx + xLo);
+        StdDraw.setYscale(-0.05*dy + yLo, 1.05*dy + yLo);
+        StdDraw.rectangle(xLo + dx/2.0, yLo + dy/2.0, dx/2.0, dy/2.0);
+        StdDraw.show();
+    }
+    
+    public void draw(Function<Double, Double> f, double delta) {
+        double x = xLo;
+        double y;
+        double xLast = Double.NaN;
+        double yLast = Double.NaN;
+        while (x < xHi) {
+            try {
+                y = f.apply(x);
+            } catch (Exception e) {
+                continue;
+            }
+            if (yLast != Double.NaN) {
+                if (y <= yHi) StdDraw.line(xLast, yLast, x, y);
+                xLast = x;
+                yLast = y;
+            }
+            x += delta;
+        }
+        StdDraw.show();
+    }
+    
+    public void discrete(Function<Double, Double> f, double delta) {
+        double x = xLo;
+        double y;
+        while (x < xHi) {
+            try {
+                y = f.apply(x);
+            } catch (Exception e) {
+                continue;
+            }
+            StdDraw.point(x, y);
+            x += delta;
+        }
+        StdDraw.show();
+    }
+    
+    public void discrete(Function<Double, Double> f, double delta, double size) {
+        double x = xLo;
+        double y;
+        while (x < xHi) {
+            try {
+                y = f.apply(x);
+            } catch (Exception e) {
+                continue;
+            }
+            StdDraw.filledCircle(x, y, size);
+            x += delta;
+        }
+        StdDraw.show();
+    }
+    
+    public static void main(String[] args) {
+        
+        Plot plot = new Plot();
+        plot.draw((x -> Math.sqrt(1.0-x*x)), 0.01);
     }
     
 }
