2a6394c3d9ba31d16c6f61383a7724c7d3406aeb
gvandomelen19 <gvandomelen19@S-RM44-07.jesuitpdx.org>
2017-10-23 09:39:58 -0700

    3Sum

--- src/datastructuresprojects/threesum/ThreeSumFast.java
+++ src/datastructuresprojects/threesum/ThreeSumFast.java
@@ -11,12 +11,14 @@ package datastructuresprojects.threesum;
  */
 import edu.princeton.cs.algs4.BinarySearch;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 import java.util.Arrays;
 
 public class ThreeSumFast {
 
     public static int count(int[] a) { // Count triples that sum to 0.
+        
         Arrays.sort(a);
         int n = a.length;
         int count = 0;
@@ -32,31 +34,72 @@ public class ThreeSumFast {
 
     public static void main(String[] args) {
         
-        In in;
         int[] ints;
-        int n;
+        double[] time = new double[5];
         double t;
         
-        for (int i = 1; i <= 32; i = i << 1) {
+        for (int i = 0; i < 5; i++) {
             
-            in = new In(String.format("3sum\\%dKints.txt", i));
-            ints = in.readAllInts();
+            int num = 1 << i;
+            
+            ints = new In(String.format("3sum\\%dKints.txt", num)).readAllInts();
             Stopwatch sw = new Stopwatch();
-            n = count(ints);
+            count(ints);
             t = sw.elapsedTime();
             
-            System.out.printf("%dKints : %f seconds%n", i, t);
+            time[i] = t;
+            
+            if (i > 0) StdOut.printf("%9s+%f seconds", "", time[i-1]);
+            if (i > 1) StdOut.printf("(*%f)", t/time[i-1]);
+            StdOut.printf("%n%2dKints : %f seconds%n", num, t);
+            
             
         }
         
-        in = new In("3sum\\1Mints.txt");
-        ints = in.readAllInts();
+        ints = new In("3sum\\1Mints.txt").readAllInts();
         Stopwatch sw = new Stopwatch();
-        n = count(ints);
+        count(ints);
         t = sw.elapsedTime();
-
-        System.out.printf("1Mints : %f seconds%n", t);
-
+        
+        StdOut.printf("%n1Mints : %f seconds%n", t);
+        
+            
+        // Arrays.sort
+        // Prediction (64K): 15 seconds
+        // Prediction (1M): 45 seconds
+        
+        // Insertion sort
+        // Prediction (64K): 16 seconds
+        // Prediction (1M): 45 seconds
         
     }
+    
+    public void insertionSort(int[] a) {
+        int n = a.length;
+        int swaps = 0;
+        for (int i = n-1; i > 0; i--) {
+            if (a[i] < a[i-1]) {
+                swap(a, i, i-1);
+                swaps++;
+            }
+        }
+        if (swaps == 0) return;
+        
+        for (int i = 2; i < n; i++) {
+            int v = a[i];
+            int j = i;
+            while (v < a[j-1]) {
+                a[j] = a[j-1];
+                j--;
+            }
+            a[j] = v;
+        }
+    }
+    
+    private void swap(int[] a, int x, int y) {
+        a[x] = a[x] ^ a[y];
+        a[y] = a[x] ^ a[y];
+        a[x] = a[x] ^ a[y];
+    }
+    
 }
